3fd58fa631e7a68b0d585f227deefb429af095b3
Michael Rodriguez <mikeyrod22@gmail.com>
2022-03-13 18:08:19 -0500

    add highlighted active nav link

--- hosterr-waitlist-page/assets/script.js
+++ hosterr-waitlist-page/assets/script.js
@@ -1,3 +1,4 @@
+// Toggle Dropdown
 const dropdownMenu = document.querySelector(".dropdown-menu")
 const menuToggle = document.querySelector("#menu-toggle")
 const hamburger = document.querySelector(".hamburger")
@@ -13,3 +14,21 @@ const handleToggle = () => {
 }
 
 menuToggle.addEventListener("click", handleToggle)
+
+// Highlight Current Nav Link
+const navLinks = Array.from(document.querySelectorAll(".nav-links > a"))
+
+const signInLinks = document.querySelectorAll(".call-to-action > a:first-child")
+
+signInLinks.forEach(link => navLinks.push(link))
+
+const handleActiveNavLink = () => {
+  navLinks.forEach(link => {
+    link.hash === window.location.hash
+      ? link.classList.add("active")
+      : link.classList.remove("active")
+  })
+}
+
+window.addEventListener("load", handleActiveNavLink)
+window.addEventListener("hashchange", handleActiveNavLink)
