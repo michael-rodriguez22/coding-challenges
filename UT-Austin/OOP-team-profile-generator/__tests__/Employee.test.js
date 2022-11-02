6dd1b890b92f29fcaec799632dceaeff877215c5
Michael Rodriguez <mikeyrod22@gmail.com>
2022-11-01 21:27:45 -0500

    add initial employee testing suite

---
+++ UT-Austin/OOP-team-profile-generator/__tests__/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require("../lib")
+
+const employeeData = {
+  name: "Michael Rodriguez",
+  id: 123456,
+  email: "michaelrodriguez22.work@gmail.com",
+}
+
+const { name, id, email } = employeeData
+
+test("tests creation of Employee object", () => {
+  const employee = new Employee(name, id, email)
+
+  expect(employee.name).toEqual(name)
+  expect(employee.id).toEqual(id)
+  expect(employee.email).toEqual(email)
+})
+
+test("getter functions return correct values", () => {
+  const employee = new Employee(name, id, email)
+
+  expect(employee.getName()).toEqual(name)
+  expect(employee.getId()).toEqual(id)
+  expect(employee.getEmail()).toEqual(email)
+})
+
+test("get role returns Employee", () => {
+  const employee = new Employee(name, id, email)
+
+  expect(employee.getRole()).toEqual("Employee")
+})
+
+module.exports = employeeData
